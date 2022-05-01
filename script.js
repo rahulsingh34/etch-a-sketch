79638aabdaf2269ec0bb595752b2f1282fbb0be1
rahulsingh34 <rahulsingh342000@gmail.com>
2022-05-01 14:20:43 -0400

    Flexible grid added clear button added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-for (i = 0; i < 256; i++) {
+const grid = document.getElementById('etch');
+
+for (i = 0; i < 16; i++) {
 	let gridBox = document.createElement('div');
 	gridBox.classList.add('grid-box');
 	document.getElementById('etch').appendChild(gridBox);
@@ -11,3 +13,24 @@ gridBoxes.forEach((gridBox) => {
 		gridBox.classList.add('hovered');
 	});
 });
+
+let slider = document.getElementById("slider");
+let slider_output = document.getElementById("output");
+slider_output.innerText = slider.value + ' x ' + slider.value;
+
+slider.oninput = function () {
+	slider_output.innerText = this.value + ' x ' + this.value;
+	grid.innerHTML = ' ';
+	for (i = 0; i < parseInt(this.value); i++) {
+		let gridBox = document.createElement('div');
+		gridBox.classList.add('grid-box');
+		document.getElementById('etch').appendChild(gridBox);
+	}
+	let gridBoxes = document.querySelectorAll('.grid-box');
+
+	gridBoxes.forEach((gridBox) => {
+	gridBox.addEventListener('mouseenter', () => {
+		gridBox.classList.add('hovered');
+	});
+});
+}
